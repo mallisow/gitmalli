29f7ad69265f8cec14cc4dda7f91bd8c5b4d4948
mallisow <Pandu@LAPTOP-2QSP4N3J>
2021-07-05 17:36:08 +0530

    name added

--- GetDemoProjectV1/src/Hellow.java
+++ GetDemoProjectV1/src/Hellow.java
@@ -4,6 +4,7 @@ public class Hellow {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is demp project V1");
+		System.out.println("malli");
 
 	}
 
